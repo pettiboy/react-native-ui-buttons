ca9bda66646512336e2a0291f4931c6bf98d447b
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-09-22 10:44:24 +0530

    🎉 first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView></ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
