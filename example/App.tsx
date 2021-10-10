670a7dc05f9ac3a9beb669dd3d0c3e2e972636a7
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-10-11 00:28:49 +0530

    ✨ added example app

---
+++ example/App.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView style={{}}></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
