9cb1dad5a1fe7c5d425bb08e483861a315a520b4
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-10-11 10:53:18 +0530

    🔨 updated test project

---
+++ TestApp/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>example</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
