62b65f69ba9023247f07e4ac2abdbd5db00bbed8
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-10-11 11:41:31 +0530

    🎨 added required files for package

--- TestApp/App.tsx
+++ TestApp/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {ButtonOutline} from 'react-native-ui-buttons';
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <SafeAreaView>
-      <Text>example</Text>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Button Outline</Text>
+        <ButtonOutline title={'Test Button'} />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  heading: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 30,
+  },
+  container: {
+    padding: 20,
+  },
+});
 
 export default App;
